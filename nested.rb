abe0a981222ac23d03cc90618bc63579661e1edd
Nick <nick.moi@gmail.com>
2019-09-20 21:20:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,12 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+<<<<<<< HEAD
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
+=======
+     programmer_hash = "GUI"
+>>>>>>> 7c15d941e328825a34a8f6899851b135a3352091
 end
 
 def adding_to_dennis
@@ -126,6 +130,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+<<<<<<< HEAD
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
+=======
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+>>>>>>> 7c15d941e328825a34a8f6899851b135a3352091
 end
